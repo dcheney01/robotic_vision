207b9d7d52719140026fb07731773acf9e22dcef
Daniel Cheney <daniel.cheney01@gmail.com>
2024-02-06 12:54:36 -0700

    finished hw3

--- hw3/src/augment_data.py
+++ hw3/src/augment_data.py
@@ -8,23 +8,25 @@ data_path = '/home/daniel/software/robotic_vision/hw3/data/Oyster Shell/train'
 transform = A.Compose([
     A.HorizontalFlip(p=0.3),
     A.VerticalFlip(p=0.3),
-    A.RandomRotate90(p=0.3),
-    A.InvertImg(p=0.1),
-    A.RandomCrop(height=480, width=360, p=0.4),
+    A.RandomRotate90(p=0.4),
+    A.InvertImg(p=0.5),
+    A.GaussNoise(p=0.3),
+    A.GaussianBlur(p=0.3),
 ])
 
-
+imgs_count = 0
 for img_folder in os.listdir(data_path):
     for img in os.listdir(os.path.join(data_path, img_folder)):
         if img.split('.')[-1] == 'png' or img.split('.')[-1] == 'tif':
             original_image = cv.imread(os.path.join(data_path, img_folder, img))
             original_image = cv.cvtColor(original_image, cv.COLOR_BGR2RGB)
-            for i in range(5):
+            for i in range(4):
                 transformed = transform(image=original_image)
                 transformed_image = transformed['image']
                 new_img_name = img.split('.')[0] + '_aug' + str(i) + '.jpg'
                 cv.imwrite(os.path.join(data_path, img_folder, new_img_name), transformed_image)
+                imgs_count += 1
         
-
+print(f"Total images created: {imgs_count}")
         # if img.split('.')[-1] == "jpg":
         #     os.remove(os.path.join(data_path, img_folder, img))
